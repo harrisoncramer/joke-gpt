c337b9dc92ebb0a5e6482c640e974b5d76a07e61
Harrison Cramer <harrisoncramer@gmail.com>
2024-08-25 09:42:17 -0400

    File renaming, deleted old logs file

--- app/types.go
+++ app/types.go
@@ -1,12 +1,5 @@
 package app
 
-import tea "github.com/charmbracelet/bubbletea"
-
-type NestedView interface {
-	tea.Model
-	back(msg tea.Msg) (tea.Model, tea.Cmd)
-}
-
 type errMsg struct{ err error }
 
 func (e errMsg) Error() string { return e.err.Error() }
